bfb9d81dd4b9908f9e25013cbb850977337ff413
divyamodi128 <modidivya16@gmail.com>
2017-02-15 16:59:00 -0800

    Update sum_of_sublist.py

--- sum_of_sublist.py
+++ sum_of_sublist.py
@@ -17,3 +17,9 @@ def maxSubLstSum(lst,n):
     return max
 
 print(maxSubLstSum(lst,len(lst)))
+
+"""
+I have solved this problem not using any sublist or tuple, because it will occupy more memory.
+
+So instead of that i have used 2 pointer varibles max and newmax.
+"""
